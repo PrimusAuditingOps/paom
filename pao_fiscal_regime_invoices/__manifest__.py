502e79e3ae2320804cf1c84b1d6d58830cef96ff
Samuel Castro <scastro@pao-mx.com>
2023-04-01 09:20:02 -0700

    PAO Fiscal Regime

---
+++ pao_fiscal_regime_invoices/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PAO Fiscal Regime',
+    'version': '1.0',
+    'author': 'samuel castro',
+    'category': '',
+    'website': 'https://paomx.com',
+    'depends': ['base','account','l10n_mx_edi_40','web'
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        'reports/account_move_report.xml',
+        'reports/external_layout_boxed.xml',
+        # views
+    ],
+}
