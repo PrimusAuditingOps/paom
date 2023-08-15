0682f4e62bc50797dc27aa53448c6fa568f8f1ca
Samuel Castro <scastro@pao-mx.com>
2023-08-14 22:28:02 -0700

    test

---
+++ pao_accounting_report/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PAO Accounting Report',
+    'version': '1.0',
+    'author': 'samuel castro',
+    'category': '',
+    'website': 'https://paomx.com',
+    'depends': ['base','account_reports'
+    ], 
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+    ]
+}
---
+++ pao_accounting_report/models/__init__.py
@@ -0,0 +1,1 @@
+from . import report_account_aged_partner_inherit
