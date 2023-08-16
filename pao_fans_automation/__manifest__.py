23303e3a5084e4847c33dbb7707337ba27324905
abrs2 <abarrios@pao-mx.com>
2023-08-16 14:12:39 -0700

    PAO Fans Automation

---
+++ pao_fans_automation/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'PAO Fans Automation',
+    'version':'1.0',
+    'author':'Abrahan Barrios',
+    'category':'',
+    'website':'https://paomx.com',
+    'depends':['sale','base','portal','servicereferralagreement',
+    ],
+    'data':[
+        #security
+        'security/ir.model.access.csv',
+        #data
+        #demo
+        #reports
+        #views
+        'views/aplications_menus.xml',
+        'views/global_gap_application_views.xml',
+        'views/website_form.xml',
+        'views/templates.xml',
+        'views/sale_order_views.xml',
+        'views/cursojavascript.xml',
+
+    ],
+    'application':'True',     
+    
+}
