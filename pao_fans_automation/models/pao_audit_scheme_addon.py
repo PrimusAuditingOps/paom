bd07ba978a918af2c9876e31be4dd6f64daf6c87
abrs2 <abarrios@pao-mx.com>
2023-08-16 09:25:29 -0700

    PAO Fans Automation

--- pao_fans_automation/models/pao_audit_scheme_addon.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-class PaoAuditSchemeAddon(models.Model):
-    _name = "pao.audit.scheme.addon"
-    _description = "PAO Audit Scheme Addon"
-    _rec_name = 'name'
-
-    name = fields.Char("Name")
