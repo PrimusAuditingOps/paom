fa6da7189868b8665b82a3b74dca1eb895e0648d
Samuel Castro <scastro@pao-mx.com>
2024-01-02 15:52:38 -0700

    test

--- pao_globalgap_fans/static/src/js/pao_globalgap_production_site.js
+++ pao_globalgap_fans/static/src/js/pao_globalgap_production_site.js
@@ -227,6 +227,9 @@ odoo.define('pao_globalgap_fans.globalgapproductionsite', function (require) {
                 data: [],
             }).render(document.getElementById("gridProducts"))
 
+            var text = $("#sites").val();
+            console.log(text);
+
             return this._super.apply(this, arguments);
         },
 
