c699f44fe590aa996839bb45ead47807fb7bac53
Samuel Castro <scastro@pao-mx.com>
2024-01-09 14:23:37 -0700

    test

--- pao_globalgap_fans/models/pao_globalgap_fans_request.py
+++ pao_globalgap_fans/models/pao_globalgap_fans_request.py
@@ -139,6 +139,6 @@ class PaoGlobalgapFansRequest(models.Model):
             'target': 'new',
             'context': {
                 'default_fans_request_id': self.id,
-                'default_capturist_id': self.capturist_id
+                'default_capturist_id': self.capturist_id.id
             }
         }
