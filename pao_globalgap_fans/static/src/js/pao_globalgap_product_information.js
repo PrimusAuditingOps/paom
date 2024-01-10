58bdffb7c4748419207948b43cbfcb0967afc6a7
Samuel Castro <scastro@pao-mx.com>
2024-01-10 02:41:12 -0700

    test

--- pao_globalgap_fans/static/src/js/pao_globalgap_product_information.js
+++ pao_globalgap_fans/static/src/js/pao_globalgap_product_information.js
@@ -106,10 +106,40 @@ odoo.define('pao_globalgap_fans.globalgapproductinformation', function (require)
                 console.log(data);
                 data.data.forEach(function(objdata) {
                     console.log(objdata);
+                    obj = {
+                        "product_id": objdata.product_id,
+                        "product_name": objdata.product_name,
+                        "uncovered_production_area": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "covered_production_area": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "applicable_harvest": gridjs.html(`<select optional="false" id="1" name="organization_buys_product">
+                        </select>`),
+                        "harvest_type": gridjs.html(`<select optional="false" id="1" name="organization_buys_product">
+                        </select>`),
+                        "product_handling": gridjs.html(`<select optional="false" id="1" name="organization_buys_product">
+                        </select>`),
+                        "outsourced_activities": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "ggn_gln_outsourced": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "product_manipulated_not_certificate": gridjs.html(`<select optional="false" id="1" name="organization_buys_product">
+                        </select>`),
+                        "organization_buys_product": gridjs.html(`<select optional="false" id="1" name="organization_buys_product">
+                        </select>`),
+                        "estimated_yield_in_tons": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "dates_harvest_estimated": gridjs.html(`<input type="text" id="samuel21"/>`),
+                        "countries_of_products":  gridjs.html(`<select id="sasa" class="chzn-select" multiple="true" name="faculty" style="width:200px;">
+                                                                     <option value="AC">A</option>
+                                                                     <option value="AD">B</option>
+                                                                     <option value="AM">C</option>
+                                                                     <option value="AP">D</option>
+                                                                 </select>`)
+                     };
+                     this.data.push(obj);
                 });
+                this.grid_selector.updateConfig({
+                    data: this.datas
+                }).forceRender();
 
             });
-
+/*
             this.datas = [
                 {
                    "product_id": 13,
@@ -137,14 +167,12 @@ odoo.define('pao_globalgap_fans.globalgapproductinformation', function (require)
                                                                 <option value="AP">D</option>
                                                             </select>`)
                 }
-            ];
+            ];*/
             this.products = []
 
 
 
-            this.grid_selector.updateConfig({
-                data: this.datas
-            }).forceRender();
+           
 
 
            
