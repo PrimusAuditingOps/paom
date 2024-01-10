4295233170cae225b25a9d239e16959c1e406df6
Samuel Castro <scastro@pao-mx.com>
2024-01-10 01:54:59 -0700

    test

--- pao_globalgap_fans/static/src/js/pao_globalgap_product_information.js
+++ pao_globalgap_fans/static/src/js/pao_globalgap_product_information.js
@@ -108,10 +108,8 @@ odoo.define('pao_globalgap_fans.globalgapproductinformation', function (require)
                 'fan_id': $("#fr_id").val().trim(), 
                 'fan_token': $("#fr_token").val().trim(), 
             }).then(function (data) {
-                console.log(data);
-                data.data.forEach(element => {
-                    console.log(element);
-                });
+                console.log(data.data);
+                
 
             });
 
