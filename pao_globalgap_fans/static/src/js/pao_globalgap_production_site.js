9a83d2625756585cf85619d58f718f34bde173be
Samuel Castro <scastro@pao-mx.com>
2024-02-29 11:02:40 -0700

    test

--- pao_globalgap_fans/static/src/js/pao_globalgap_production_site.js
+++ pao_globalgap_fans/static/src/js/pao_globalgap_production_site.js
@@ -237,25 +237,25 @@ odoo.define('pao_globalgap_fans.globalgapproductionsite', function (require) {
                             onClick: () => {
                                 const list = this.datas.filter(d => d.index == row.cells[22].data);
                                 console.log(list);
-                                $("#productionsite").val(list.name);
-                                $("#address").val(list.address); 
-                                $("#postal_address").val(list.postal_address);
-                                $("#sitecountry").val(list.country);
-                                $("#sitestate").val(list.state); 
-                                $("#zip").val(list.zip);
-                                $("#telephone").val(list.telephone); 
-                                $("#email").val(list.email); 
-                                $("#latitude").val(list.latitude);
-                                $("#longitude").val(list.longitude);
-                                $("#contactname").val(list.contactname); 
-                                $("#contactaddress").val(list.contactaddress); 
-                                $("#contaccountry").val(list.contaccountry); 
-                                $("#contactstate").val(list.contactstate); 
-                                $("#contaccity").val(list.contactcity); 
-                                $("#contactzip").val(list.contactzip); 
-                                $("#contacttelephone").val(list.contacttelephone);
-                                $("#contactemail").val(list.contactemail);
-                                $("#site_index").val(list.index);
+                                $("#productionsite").val(list[0].name);
+                                $("#address").val(list[0].address); 
+                                $("#postal_address").val(list[0].postal_address);
+                                $("#sitecountry").val(list[0].country);
+                                $("#sitestate").val(list[0].state); 
+                                $("#zip").val(list[0].zip);
+                                $("#telephone").val(list[0].telephone); 
+                                $("#email").val(list[0].email); 
+                                $("#latitude").val(list[0].latitude);
+                                $("#longitude").val(list[0].longitude);
+                                $("#contactname").val(list[0].contactname); 
+                                $("#contactaddress").val(list[0].contactaddress); 
+                                $("#contaccountry").val(list[0].contaccountry); 
+                                $("#contactstate").val(list[0].contactstate); 
+                                $("#contaccity").val(list[0].contactcity); 
+                                $("#contactzip").val(list[0].contactzip); 
+                                $("#contacttelephone").val(list[0].contacttelephone);
+                                $("#contactemail").val(list[0].contactemail);
+                                $("#site_index").val(list[0].index);
                                 
 
                                 /*
