40f3ff096ac6598e0977549c392b08d26f703a33
UzuetaPAO <muzueta@pao-mx.com>
2024-03-15 15:03:41 -0700

    [FIX] pao_pricelist_proposal

--- pao_pricelist_proposal/models/pao_pricelist_proposal.py
+++ pao_pricelist_proposal/models/pao_pricelist_proposal.py
@@ -80,6 +80,9 @@ class PriceListProposal(models.Model):
     def send_proposal_action(self):
         if self.proposal_status == 'draft' and self.authorized:
             
+            if not self.pricelist_proposal_manager:
+                raise ValidationError(_("No employee in charge of pricelist proposals was found."))
+            
             if not self.pricelist_proposal_manager_id.es_sign_signature or not self.create_uid.employee_id.es_sign_signature:
                 raise ValidationError(_("Please ensure that all employees involved in this process have a signature assigned in the Employees section."))
             
