1ae6d41ca43fda4d62172aefe3d2302a4233888c
UzuetaPAO <muzueta@pao-mx.com>
2024-03-20 15:57:47 -0700

    [FIX] pao_pricelist_proposal

--- pao_pricelist_proposal/models/res_partner.py
+++ pao_pricelist_proposal/models/res_partner.py
@@ -5,9 +5,13 @@ class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
     def notify_channel_action(self, message, channel_name, subject):
+
         channel = self.env['mail.channel'].search([('name', '=', channel_name)], limit=1)
         
+        if channel:
+            channel.message_post(body=message)
+            
         message = self.message_post(
             body=message,
-            partner_ids=channel.channel_partner_ids,
+            partner_ids=channel.channel_partner_ids.ids,
         )
