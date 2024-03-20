545068b72ebe5d0a40ace44938a601ca7cc95012
UzuetaPAO <muzueta@pao-mx.com>
2024-03-20 15:44:23 -0700

    [FIX] pao_pricelist_proposal

--- pao_pricelist_proposal/models/res_partner.py
+++ pao_pricelist_proposal/models/res_partner.py
@@ -5,18 +5,9 @@ class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
     def notify_channel_action(self, message, channel_name, subject):
-        # self.message_post(
-        #     body=message, 
-        #     subject="New message added"
-        # )
+        channel = self.env['mail.channel'].search([('name', '=', channel_name)], limit=1)
         
         message = self.message_post(
             body=message,
+            partner_ids=channel.channel_partner_ids,
         )
-
-        channel = self.env['mail.channel'].search([('name', '=', channel_name)], limit=1)
-
-        if channel:
-            # Notify all members of the channel
-            for member in channel.channel_partner_ids:
-                member.message_post(body=message, subject=subject)
