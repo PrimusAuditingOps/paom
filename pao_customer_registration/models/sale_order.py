4c12080d6355cf8a0dfe5ecd3d617711a84dd4f5
Samuel Castro <scastro@pao-mx.com>
2024-07-15 09:17:34 -0700

    Se actualiza modulo de customer registration.

--- pao_customer_registration/models/sale_order.py
+++ pao_customer_registration/models/sale_order.py
@@ -8,31 +8,29 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        for rec in self:
-            partner = vals.get('partner_id')
-            if partner: 
-                recPartner = rec.env["res.partner"].browse(partner)
-                for rec in recPartner:
-                    recRegistration = rec.env["pao.customer.registration"].search([("res_partner_id", "=", rec.id)])
-                    if not recRegistration:
-                        cr = rec.env['pao.customer.registration'].create({
-                            'contact_id': rec.id,
-                            'res_partner_id': rec.id,
-                            'name': rec.name,
-                            'country_id': rec.country_id.id,
-                            'state_id': rec.state_id.id,
-                            'city_id': rec.city_id.id,
-                            'street_name': rec.street,
-                            'street_number': rec.street2,
-                            'zip': rec.zip,
-                            'phone': rec.phone,
-                            'email': rec.email,
-                            'fiscal_regime': rec.l10n_mx_edi_fiscal_regime,
-                            'vat': rec.vat,
-                        })
-                        base_url = rec.env['ir.config_parameter'].sudo().get_param('web.base.url')
-                        form_url = url_join(base_url, '/pao/customer/registration/%s/%s' % (cr.id, cr.access_token))
-                        cr.write({"form_url": form_url})
-            records = super(SaleOrder, rec).create(vals)
-            
-            return records
+        partner = vals.get('partner_id')
+        if partner:
+            recPartner = self.env["res.partner"].browse(partner)
+            for recP in recPartner:
+                recRegistration = self.env["pao.customer.registration"].search([("res_partner_id", "=", recP.id)])
+                if not recRegistration:
+                    cr = self.env['pao.customer.registration'].create({
+                        'contact_id': recP.id,
+                        'res_partner_id': recP.id,
+                        'name': recP.name,
+                        'country_id': recP.country_id.id,
+                        'state_id': recP.state_id.id,
+                        'city_id': recP.city_id.id,
+                        'street_name': recP.street,
+                        'street_number': recP.street2,
+                        'zip': recP.zip,
+                        'phone': recP.phone,
+                        'email': recP.email,
+                        'fiscal_regime': recP.l10n_mx_edi_fiscal_regime,
+                        'vat': recP.vat,
+                    })
+                    base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+                    form_url = url_join(base_url, '/pao/customer/registration/%s/%s' % (cr.id, cr.access_token))
+                    cr.write({"form_url": form_url})
+        records = super(SaleOrder, self).create(vals)
+        return records
