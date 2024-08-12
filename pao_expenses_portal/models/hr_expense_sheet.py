b25d76f10bc53baee59ea2ed5badf4fb04f997dd
Manuel Uzueta <muzueta@pao-mx.com>
2024-08-12 12:03:42 -0700

    test

---
+++ pao_expenses_portal/models/hr_expense_sheet.py
@@ -0,0 +1,139 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ExpenseSheetInherit(models.Model):
+    _inherit = 'hr.expense.sheet'
+    
+    purchase_order = fields.Many2one('purchase.order', string='Purchase Order')
+    partner_id = fields.Many2one('res.partner', string='Contact')
+    expense_scheme_id = fields.Many2one('expense.scheme', string='Scheme', default=None)
+    
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string='Employee',
+        required=False,  # This line disables the required constraint
+        readonly=False,
+        check_company=True,
+        tracking=True,
+    )
+    
+    @api.model
+    def action_sheet_move_create(self):
+        
+        if not self.partner_id and not self.employee_id:
+            raise ValidationError(_("You must define a Contact or Employee."))
+        
+        if self.partner_id and self.partner_id.ado_is_auditor and not self.partner_id.st_supplier_taxes_id:
+            raise ValidationError(_("The Contact must have the supplier taxes defined."))
+        
+        result = super(ExpenseSheetInherit, self).action_sheet_move_create()
+        
+        for move in self.account_move_ids:
+            for line in move.line_ids:
+                line.partner_id = line.expense_id.partner_id
+            
+        return result
+    
+    @api.model 
+    def write(self, values):
+        result = super(ExpenseSheetInherit, self).write(values)
+        
+        for record in self:
+            if record.expense_line_ids and record.expense_scheme_id:
+                for expense in record.expense_line_ids:
+                    expense.sudo().account_id = record.expense_scheme_id.property_account_expense_id.id
+            
+        return result
+    
+    @api.model
+    def _prepare_bills_vals(self):
+        
+        bills_vals = super(ExpenseSheetInherit, self)._prepare_bills_vals()
+        
+        partner_id = self.employee_id.sudo().work_contact_id.id if self.employee_id else self.partner_id.id
+        # partner_id = self.partner_id.id
+        bills_vals.update({'partner_id': partner_id})
+        
+        return bills_vals
+    
+    @api.constrains('partner_id')
+    def _check_scheme(self):
+        for record in self:
+            if record.partner_id.ado_is_auditor and not record.expense_scheme_id:
+                raise ValidationError(_("You must define a Scheme."))
+    
+class ExpenseInherit(models.Model):
+    _inherit = "hr.expense"
+    
+    partner_id = fields.Many2one('res.partner', string='Contact', required=True, domain=[('ado_is_auditor', '=', False)])
+
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string='Employee',
+        default=None,
+        required=False,  # This line disables the required constraint
+        readonly=False,
+        check_company=True,
+        domain=[('filter_for_expense', '=', True)],
+        tracking=True,
+    )
+    
+    uploaded_by_statement = fields.Boolean(default=False)
+    
+    @api.model
+    def _prepare_move_lines_vals(self):
+        
+        vals = super(ExpenseInherit, self)._prepare_move_lines_vals()
+        
+        expense_name = self.name.split('\n')[0][:64]
+        vals.update({'name': f'{self.employee_id.name if self.employee_id else self.partner_id.name}: {expense_name}',})
+        
+        return vals
+    
+    @api.model
+    def write(self, values):
+        show_warning = False
+        for record in self:
+            
+            if 'sheet_id' in values:
+                required_fields = ['product_id', 'partner_id', 'date']
+                missing_fields = []
+            
+                # Check which of the required fields are missing in the current record
+                for field in required_fields:
+                    if not values.get(field) and not getattr(record, field):
+                        missing_fields.append(field)
+                
+                if missing_fields:
+                    values['sheet_id'] = None
+                    show_warning = True
+                    
+            result = super(ExpenseInherit, self).write(values)
+            
+            if ('state' in values and values['state'] == 'reported') or ('sheet_id' in values and values['sheet_id']):
+                activities = self.env['mail.activity'].search([
+                        ('res_model', '=', 'hr.expense'),
+                        ('res_id', 'in', self.ids),
+                        ('user_id', '=', self.env.user.id)
+                    ])
+                activities.action_done()
+            
+            if not record.employee_id and record.partner_id and not record.partner_id.ado_is_auditor:
+                raise ValidationError(_("You must define an Employee."))
+        
+        if show_warning:
+            self.env['bus.bus']._sendone(self.env.user.partner_id, 'simple_notification', {
+                'type': 'warning',
+                'title': _("Warning"),
+                'message': _('Expenses must be completed before adding them to a report.')
+            })   
+        return result
+    
+    def unlink(self):
+        for record in self:
+            if record.uploaded_by_statement and not self.env.user.has_group('base.group_system'):
+                raise UserError(_("You cannot delete an expense that was uploaded via a bank statement."))
+        return super(ExpenseInherit, self).unlink()
---
+++ pao_expenses_portal/models/upload_statement.py
@@ -0,0 +1,216 @@
+from odoo import models, fields, api, _
+import base64
+import xlrd
+import csv
+from datetime import datetime, timedelta
+import dateutil.parser
+from odoo.exceptions import UserError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class UploadExpenseStatement(models.TransientModel):
+    _name = 'upload.expense.statement'
+    _description = 'Upload Expense Statement'
+
+    employee_id = fields.Many2one('hr.employee')
+    statement_file = fields.Binary('Account Statement', required=True)
+    statement_filename  = fields.Char('File Name')
+
+    def write(self, vals):
+        try:
+            if 'statement_file' in vals:
+                vals['statement_filename'] = vals.get('statement_file', '').split('/')[-1] or 'unknown_filename'
+        except Exception as e:
+            action= {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'type': 'danger',
+                    'title': _('Error Uploading Statement'),
+                    'message': _('An error occurred while processing the file, please try again.'),
+                    'sticky': True,
+                    'next': {'type': 'ir.actions.act_window_close'},
+                }
+            }
+            return action
+        return super(UploadExpenseStatement, self).write(vals)
+    
+    def action_import_statement(self):
+        try:
+            send_notification_to_user = False
+            odoo_bot = self.env.ref('base.partner_root')
+            
+            if not self.employee_id:
+                self.employee_id = self.env.user.employee_id.id
+            else:
+                send_notification_to_user = True
+            
+            if self.env.company.country_code not in ('MX', 'US'):
+                raise UserError(_('This process is not available for your company. Please contact IT support.'))
+            
+            
+            if '.csv' in self.statement_filename:
+                expenses = self._process_csv_file(base64.b64decode(self.statement_file))
+            elif '.xlsx' in self.statement_filename:
+                expenses = self._process_excel_file(base64.b64decode(self.statement_file))
+            else:
+                raise UserError(_('Unsupported file format. Please upload a CSV or Excel file with a correct format.'))
+            
+            for expense in expenses:
+                if send_notification_to_user:
+                    self.env['mail.activity'].create({
+                        'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+                        'summary': _('New expense to complete and send for approval'),
+                        'note': _('You have a new expense to complete and send for approval.'),
+                        'res_model_id': self.env['ir.model'].search([('model', '=', 'hr.expense')], limit=1).id,
+                        'res_id': expense.id,
+                        'user_id': expense.employee_id.user_id.id,
+                        'create_uid': odoo_bot.id,
+                    })
+        except UserError as e:
+            _logger.warning(e)
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'type': 'danger',
+                    'title': _('Error Uploading Statement'),
+                    'message': e,
+                    'sticky': True,
+                    'next': {'type': 'ir.actions.act_window_close'},
+                }
+            }
+        
+        return {'type': 'ir.actions.client', 'tag': 'reload'}
+    
+    def _process_csv_file(self, file_content):
+        reader = csv.reader(file_content.decode('utf-8').splitlines())  # Decode for CSV handling
+
+        expenses = []
+
+        if self.env.company.country_code == 'MX':
+            first_row = 8
+        elif self.env.company.country_code == 'US':
+            first_row = 1
+            
+        for x in range(first_row-1):
+            next(reader)
+        
+        row_index=0
+        for row in reader:
+            
+            row_index += 1
+            if row_index == 1:
+                headers = row
+                if not self._check_format(headers):
+                    raise UserError(_('The format of the uploaded file is incorrect, please try again'))
+                continue
+            
+            if self.env.company.country_code == 'MX':
+                expense = self._process_mexico_format(row)
+            elif self.env.company.country_code == 'US':
+                expense = self._process_usa_format(row)
+                
+            if expense:   
+                expenses.append(expense)
+
+        return expenses
+
+    def _process_excel_file(self, file_content):
+        workbook = xlrd.open_workbook(file_contents=file_content)
+        worksheet = workbook.sheet_by_index(0)  # Assuming data is in the first sheet
+        
+        expenses = []
+        
+        
+        if self.env.company.country_code == 'MX':
+            first_row = 8
+        elif self.env.company.country_code == 'US':
+            first_row = 1
+            
+        headers = worksheet.row_values(first_row-1)
+        
+        if not self._check_format(headers):
+            raise UserError(_('The format of the uploaded file is incorrect, please try again'))
+        
+        # Process the Excel data and create records
+        for rownum in range(first_row, worksheet.nrows):
+            row = worksheet.row_values(rownum)
+            if self.env.company.country_code == 'MX':
+                expense = self._process_mexico_format(row)
+            elif self.env.company.country_code == 'US':
+                expense = self._process_usa_format(row)
+                
+            if expense:   
+                expenses.append(expense)
+        
+        return expenses
+    
+    def _check_format(self, headers):
+        if self.env.company.country_code == 'MX':
+            placeholder = ['SEC','Concepto/Referencia','Cargo','Abono','Saldo']
+        elif self.env.company.country_code == 'US':
+            placeholder = ['Date','Transaction','Name','Memo','Amount']
+        
+        for header, expected in zip(headers, placeholder):
+            _logger.warning(header)
+            _logger.warning(expected)
+            if header.strip() != expected:
+                return False
+        
+        return True
+
+    
+    def _process_mexico_format(self, row):
+        expense = None
+        
+        if str(row[2]).strip() != '':
+            expense = self.env['hr.expense'].sudo().create({
+                'name': row[1],
+                'product_id': None,
+                'employee_id': self.employee_id.id,
+                'partner_id': None,
+                'total_amount_currency': float(str(row[2]).replace(',', '')) if row[2] else 0.0,  # Handle potential empty values
+                'currency_id': self.env.company.currency_id.id,
+                'payment_mode': 'company_account',
+                'uploaded_by_statement': True,
+                'date': None,
+            })
+        
+        return expense
+    
+    def _process_usa_format(self, row):
+        expense = None
+        
+        date_value = self._parse_date(row[0])
+        
+        if str(row[4]).strip() != '':
+            expense = self.env['hr.expense'].sudo().create({
+                'name': row[2],
+                'product_id': None,
+                'employee_id': self.employee_id.id,
+                'partner_id': None,
+                'total_amount_currency': abs(float(str(row[4]).replace(',', ''))) if row[4] else 0.0,  # Handle potential empty values
+                'currency_id': self.env.company.currency_id.id,
+                'payment_mode': 'company_account',
+                'uploaded_by_statement': True,
+                'date': date_value,
+            })
+        
+        return expense
+    
+    def _parse_date(self, value):
+        try:
+            # Check if the value is a float (likely an Excel date serial number)
+            if isinstance(value, float):
+                if value > 40000:  # Basic filter for date serial numbers
+                    excel_start_date = datetime(1899, 12, 30)
+                    delta = timedelta(days=value)
+                    return excel_start_date + delta
+            elif isinstance(value, str):
+                # Try to parse the string as a date
+                return dateutil.parser.parse(value)
+        except (ValueError, TypeError):
+            # If parsing fails, return None
+            return None
