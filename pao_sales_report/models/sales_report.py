33aad215acd76da36ff221607caea28e2b80d7f5
Manuel Uzueta <muzueta@pao-mx.com>
2024-08-16 13:20:15 -0700

    test

--- pao_sales_report/models/sales_report.py
+++ pao_sales_report/models/sales_report.py
@@ -3,11 +3,11 @@ from odoo import models, fields, api
 class SaleReportInherit(models.Model):
     _inherit="sale.report"
 
-    ship_date = fields.Date('Ship Date', readonly=True)
-    organization_id = fields.Many2one('servicereferralagreement.organization', 'Organization', readonly=True)
-    registry_number_id = fields.Many2one('servicereferralagreement.registrynumber', 'Registry Number', readonly=True)
-    audit_date = fields.Date('Audit Date', readonly=True)
-    end_date = fields.Date('End Date', readonly=True)
+    ship_date = fields.Date('Ship Date', readonly=True, default=None)
+    organization_id = fields.Many2one('servicereferralagreement.organization', 'Organization', readonly=True, default=None)
+    registry_number_id = fields.Many2one('servicereferralagreement.registrynumber', 'Registry Number', readonly=True, default=None)
+    audit_date = fields.Date('Audit Date', readonly=True, default=None)
+    end_date = fields.Date('End Date', readonly=True, default=None)
     
     def _group_by_sale(self):
         group_by_fields = super(SaleReportInherit, self)._group_by_sale()
