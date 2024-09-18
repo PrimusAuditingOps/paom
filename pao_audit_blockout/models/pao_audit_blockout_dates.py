1e5a6d9c0e2d42b80666454bdea01f3124e3dbca
Uzueta <uzueta.m@hotmail.com>
2024-09-18 16:21:39 -0700

    test

--- pao_audit_blockout/models/pao_audit_blockout_dates.py
+++ pao_audit_blockout/models/pao_audit_blockout_dates.py
@@ -1,5 +1,6 @@
 from odoo import fields, models, api, _
 from logging import getLogger
+from odoo.exceptions import ValidationError
 
 _logger = getLogger(__name__)
 
@@ -11,3 +12,9 @@ class SaleOrder(models.Model):
     start_date = fields.Date(string="Start Date", required=True)
     end_date = fields.Date(string="End Date", required=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
+
+    @api.constrains('start_date', 'end_date')
+    def _check_dates(self):
+        for record in self:
+            if record.start_date and record.end_date and record.start_date > record.end_date:
+                raise ValidationError(_('The start date must be before the end date.'))
--- pao_audit_blockout/models/sale_order.py
+++ pao_audit_blockout/models/sale_order.py
@@ -12,6 +12,6 @@ class SaleOrderInherit(models.Model):
                     if line.service_start_date and line.service_end_date:
                         blockout_dates = order.partner_id.blockout_dates_ids
                         for blockout in blockout_dates:
-                            if line.service_start_date <= blockout.end_date and line.end_start_date >= blockout.start_date:
+                            if line.service_start_date <= blockout.end_date and line.service_end_date >= blockout.start_date:
                                 raise UserError(_("You cannot schedule unannounced audits within a range of blockout dates set by the customer. (Blockout Range: %(start_date)s - %(end_date)s)") % {'start_date': blockout.start_date, 'end_date': blockout.end_date})
                             
