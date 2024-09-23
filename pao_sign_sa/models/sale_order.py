7f20f4db69a3609486fb635cb2fb6f83ec3c646c
Uzueta <uzueta.m@hotmail.com>
2024-09-23 14:25:02 -0700

    test

--- pao_sign_sa/models/sale_order.py
+++ pao_sign_sa/models/sale_order.py
@@ -25,11 +25,43 @@ class SaleOrder(models.Model):
         readonly=True,
     )   
 
+    sa_status = fields.Selection(string="SA Status", selection=[
+        ('no_sa_request', "SA Signature Not Requested"),
+        ('pending_sa', "SA Signature Pending"),
+        ('all_sign', "SA Fully Signed"),
+            ],
+        default = 'no_sa_request',
+        compute='_get_sa_status'
+    )           
 
     @api.depends('pao_agreements_ids')
     def _get_agreements(self):
         for order in self:
             order.pao_agrements_count = len(order.pao_agreements_ids)
+
+    @api.depends('pao_agreements_ids.document_status')
+    def _get_sa_status(self):
+        for order in self:
+            agreements = order.pao_agreements_ids
+            if not agreements:
+                # No agreements, so set the status to 'no_sa_request'
+                order.sa_status = 'no_sa_request'
+            else:
+                # Ignore 'cancel' status agreements
+                valid_agreements = agreements.filtered(lambda a: a.document_status != 'cancel')
+
+                if not valid_agreements:
+                    # All agreements are canceled
+                    order.sa_status = 'no_sa_request'
+                elif any(a.document_status in ('sent', 'partially_sign') for a in valid_agreements):
+                    # At least one agreement is pending signature
+                    order.sa_status = 'pending_sa'
+                elif all(a.document_status == 'sign' for a in valid_agreements):
+                    # All valid agreements are signed
+                    order.sa_status = 'all_sign'
+                else:
+                    # Default to 'no_sa_request' if none of the conditions match
+                    order.sa_status = 'no_sa_request'
 
     @api.depends('order_line')
     def _get_registration_numbers(self):
