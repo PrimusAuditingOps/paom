1b8792d8ec996fdcf5ebd9c196e0022a785c4161
Uzueta <uzueta.m@hotmail.com>
2024-09-23 09:11:01 -0700

    test

--- pao_sign_sa/controllers/portal.py
+++ pao_sign_sa/controllers/portal.py
@@ -77,8 +77,10 @@ class CustomerPortal(portal.CustomerPortal):
 
         base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
         prefix = '/sign/sa' if signer == 'customer' else '/coordinator_sign/sa'
-
-        return request.redirect(url_join(base_url, '%s/%s/%s' % (prefix, sa_id, sa_token)))
+        return {
+            'force_refresh': True,
+            'redirect_url':  url_join(base_url, '%s/%s/%s' % (prefix, sa_id, sa_token))
+        }
 
     @http.route(['/sign/sa/<int:sa_id>/<string:sa_token>', '/coordinator_sign/sa/<int:sa_id>/<string:sa_token>'], type='http', auth="public", website=True)
     def portal_sign_sa(self, report_type=None, sa_id=False, sa_token=None, download=False, **kw):
