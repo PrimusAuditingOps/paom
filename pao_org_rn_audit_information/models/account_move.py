3fbadc670b440676c85bffa22e9fe1de2ed0501f
Uzueta <uzueta.m@hotmail.com>
2024-09-24 12:31:26 -0700

    test

--- pao_org_rn_audit_information/models/account_move.py
+++ pao_org_rn_audit_information/models/account_move.py
@@ -1,7 +1,5 @@
 from odoo import api, models, fields
 from odoo.tools.translate import _
-from logging import getLogger
-_logger = getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -10,20 +8,16 @@ class ResPartner(models.Model):
     audit_date = fields.Date(string="Audit Date", compute="_compute_audit_information_fields", store=True)
     organization = fields.Char(string="Organization", compute="_compute_audit_information_fields", store=True)
     registry_number = fields.Char(string="Registry Number", compute="_compute_audit_information_fields", store=True)
-                
-    @api.depends('invoice_line_ids.sale_line_ids.order_id')
+
     def _compute_audit_information_fields(self):
-        for move in self:
-            # Fetch the related sale order if available
-            sale_orders = move.invoice_line_ids.mapped('sale_line_ids.order_id')
-            _logger.warning(sale_orders)
-            if sale_orders:
-                # Use the first sale order found, or adjust if you expect multiple orders
-                sale_order = sale_orders[0]
-                move.audit_date = sale_order.serive_start_date
-                move.organization = sale_order.organization_id.name
-                move.registry_number = sale_order.registrynumber_id.name
+        for record in self:
+            # Assuming 'invoice_origin' links to the sale order name
+            sale_order = self.env['sale.order'].search([('invoice_ids', 'in', record.id)], limit=1)
+            if sale_order:
+                record.audit_date = sale_order.serive_start_date
+                record.organization = sale_order.organization_id.name
+                record.registry_number = sale_order.registrynumber_id.name
             else:
-                move.audit_date = False
-                move.organization = False
-                move.registry_number = False
+                record.audit_date = False
+                record.organization = False
+                record.registry_number = False
