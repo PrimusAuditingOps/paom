17f221f2692e4a7e0954a45f6c48a9eac4896edd
Uzueta <uzueta.m@hotmail.com>
2024-09-24 12:36:42 -0700

    test

--- pao_org_rn_audit_information/models/account_move.py
+++ pao_org_rn_audit_information/models/account_move.py
@@ -5,14 +5,14 @@ from odoo.tools.translate import _
 class ResPartner(models.Model):
     _inherit = 'account.move'
 
-    audit_date = fields.Date(string="Audit Date", compute="_compute_audit_information_fields", store=True)
-    organization = fields.Char(string="Organization", compute="_compute_audit_information_fields", store=True)
-    registry_number = fields.Char(string="Registry Number", compute="_compute_audit_information_fields", store=True)
+    audit_date = fields.Date(string="Audit Date", compute="_compute_sale_order_related_fields", store=True)
+    organization = fields.Char(string="Organization", compute="_compute_sale_order_related_fields", store=True)
+    registry_number = fields.Char(string="Registry Number", compute="_compute_sale_order_related_fields", store=True)
 
-    @api.depends('invoice_line_ids')
-    def _compute_audit_information_fields(self):
+    @api.depends('invoice_line_ids.sale_line_ids.order_id.invoice_ids')
+    def _compute_sale_order_related_fields(self):
         for record in self:
-            # Assuming 'invoice_origin' links to the sale order name
+            # Search for the sale order that contains the current account move
             sale_order = self.env['sale.order'].search([('invoice_ids', 'in', record.id)], limit=1)
             if sale_order:
                 record.audit_date = sale_order.serive_start_date
