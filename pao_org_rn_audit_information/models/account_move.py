87143df5f333b335fe3537bd4c52839189088bbc
Uzueta <uzueta.m@hotmail.com>
2024-09-24 12:19:56 -0700

    test

--- pao_org_rn_audit_information/models/account_move.py
+++ pao_org_rn_audit_information/models/account_move.py
@@ -5,29 +5,22 @@ from odoo.tools.translate import _
 class ResPartner(models.Model):
     _inherit = 'account.move'
 
-    audit_date = fields.Date(string="Audit Date", compute="_get_audit_date", store=True)
-    organization = fields.Char(string="Organization", compute="_get_organization", store=True)
-    registry_number = fields.Char(string="Registry Number", compute="_get_registry_number", store=True)
-    
-    def _get_audit_date(self):
-        source_orders = self.line_ids.sale_line_ids.order_id
-        
-        self.audit_date = None
-        if source_orders:
-            self.audit_date = source_orders[0].service_start_date
-    
-    def _get_organization(self):
-        source_orders = self.line_ids.sale_line_ids.order_id
-        
-        self.organization = None
-        if source_orders:
-            organization = source_orders[0].organization_id
-            self.organization = organization.name if organization else None
-            
-    def _get_registry_number(self):
-        source_orders = self.line_ids.sale_line_ids.order_id
-        
-        self.registry_number = None
-        if source_orders:
-            registry_number = source_orders[0].registrynumber_id
-            self.registry_number = registry_number.name if registry_number else None
+    audit_date = fields.Date(string="Audit Date", compute="_compute_audit_information_fields", store=True)
+    organization = fields.Char(string="Organization", compute="_compute_audit_information_fields", store=True)
+    registry_number = fields.Char(string="Registry Number", compute="_compute_audit_information_fields", store=True)
+                
+    @api.depends('line_ids.sale_line_ids.order_id')
+    def _compute_audit_information_fields(self):
+        for move in self:
+            # Fetch the related sale order if available
+            sale_orders = move.line_ids.mapped('sale_line_ids.order_id')
+            if sale_orders:
+                # Use the first sale order found, or adjust if you expect multiple orders
+                sale_order = sale_orders[0]
+                move.audit_date = sale_order.audit_date
+                move.organization = sale_order.organization
+                move.registry_number = sale_order.registry_number
+            else:
+                move.audit_date = False
+                move.organization = False
+                move.registry_number = False
