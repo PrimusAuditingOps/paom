8ae306fbd6e95f98503f49f5b689d36c3fc95b32
Uzueta <uzueta.m@hotmail.com>
2024-09-24 12:37:21 -0700

    test

--- pao_org_rn_audit_information/models/account_move.py
+++ pao_org_rn_audit_information/models/account_move.py
@@ -1,5 +1,8 @@
 from odoo import api, models, fields
 from odoo.tools.translate import _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -12,6 +15,7 @@ class ResPartner(models.Model):
     @api.depends('invoice_line_ids.sale_line_ids.order_id.invoice_ids')
     def _compute_sale_order_related_fields(self):
         for record in self:
+            _logger.info("Entering _compute_sale_order_related_fields for record %s", record.id)
             # Search for the sale order that contains the current account move
             sale_order = self.env['sale.order'].search([('invoice_ids', 'in', record.id)], limit=1)
             if sale_order:
