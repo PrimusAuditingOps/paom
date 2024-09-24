69a40ac867269c77c87e75c8293c86a498940c0e
Uzueta <uzueta.m@hotmail.com>
2024-09-24 16:40:59 -0700

    test

--- pao_base/models/account_move.py
+++ pao_base/models/account_move.py
@@ -5,4 +5,4 @@ from odoo.tools.translate import _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    billing_user_id = fields.Many2one('res.users', string="Billing Person", default = lambda self: self.env.user.id)
+    billing_user_id = fields.Many2one('res.users', string="Billing Person", default = lambda self: self.create_uid.id)
