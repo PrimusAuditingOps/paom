3fa1eca7cf5e6220e233502c03fc836efde078d4
Uzueta <uzueta.m@hotmail.com>
2024-09-25 13:32:32 -0700

    test

--- pao_client_sa_inquiry/__manifest__.py
+++ pao_client_sa_inquiry/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "",
     "depends": ["contacts","pao_sign_sa"],
     'data': [
-        # 'views/account_move_inherit.xml',
+        'views/sale_order_inherit.xml',
         # 'views/hr_employeee_form_inherit.xml',
         # 'views/expense_scheme_views.xml',
         # 'views/hr_expense_sheet_view.xml',
--- pao_client_sa_inquiry/models/res_partner.py
+++ pao_client_sa_inquiry/models/res_partner.py
@@ -12,11 +12,25 @@ class ResPartnerInherit(models.Model):
         string='Agrements',
     )
     
+    pao_agrements_count = fields.Integer(
+        compute='_compute_agreements'
+    )
+    
     @api.depends('pao_agreements_ids')
     def _compute_agreements(self):
         for record in self:
-            related_greemetns = record.env['pao.sign.sa.agreements.sent'].search([('so_partner_id', 'in', [record.id] + record.child_ids.ids)])
-            record.pao_agreements_ids = related_greemetns
-
-    
-    
+            related_agreements = record.env['pao.sign.sa.agreements.sent'].search([('so_partner_id', 'in', [record.id] + record.child_ids.ids)])
+            record.pao_agreements_ids = related_agreements
+            record.pao_agrements_count = len(record.pao_agreements_ids)
+            
+    def action_view_partner_service_agreements(self):
+        self.ensure_one()     
+        action = {
+            'res_model': 'pao.sign.sa.agreements.sent',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+            'name': _("Service Agreements - %s", self.display_name),
+            # 'domain': [('so_partner_id', 'in', [self.id] + self.child_ids.ids)],
+            'domain': [('id', 'in', self.pao_agreements_ids)],
+        }
+        return action
