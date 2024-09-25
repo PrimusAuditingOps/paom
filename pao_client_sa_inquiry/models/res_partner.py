62ccf4438fa914695f9ef7cef1233d5d1d755a8e
Uzueta <uzueta.m@hotmail.com>
2024-09-25 13:20:04 -0700

    test

--- pao_client_sa_inquiry/models/res_partner.py
+++ pao_client_sa_inquiry/models/res_partner.py
@@ -8,7 +8,15 @@ class ResPartnerInherit(models.Model):
     pao_agreements_ids = fields.One2many(
         comodel_name='pao.sign.sa.agreements.sent',
         inverse_name='so_partner_id',
+        compute='_compute_agreements',
         string='Agrements',
     )
+    
+    @api.depends('pao_agreements_ids')
+    def _compute_agreements(self):
+        for record in self:
+            related_greemetns = record.env['pao.sign.sa.agreements.sent'].search([('so_partner_id', 'in', [record.id] + record.child_ids.ids)])
+            record.pao_agreements_ids = related_greemetns
+
     
     
