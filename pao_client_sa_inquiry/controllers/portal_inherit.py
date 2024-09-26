391e92becf5bc2ed72029fc824ebde0f6eda8bd5
Uzueta <uzueta.m@hotmail.com>
2024-09-26 08:54:29 -0700

    test

--- pao_client_sa_inquiry/controllers/portal_inherit.py
+++ pao_client_sa_inquiry/controllers/portal_inherit.py
@@ -5,6 +5,7 @@ import io
 import zipfile
 import base64
 from odoo.addons.web.controllers.main import content_disposition
+from collections import OrderedDict
 
 _logger = logging.getLogger(__name__)
 class ServiceAgreementsPortal(http.Controller):
@@ -13,23 +14,53 @@ class ServiceAgreementsPortal(http.Controller):
         user = request.env.user.partner_id
         return bool(user.pao_agreements_ids)
     
+    def _get_sa_searchbar_sortings(self):
+        return {
+            'date': {'label': _('Date'), 'order': 'document_date desc'},
+            'state': {'label': _('Document Status'), 'order': 'document_status'},
+        }
+        
+    def _get_sa_searchbar_filters(self):
+        return {
+            'all': {'label': _('All'), 'domain': []},
+            'pending': {'label': _('Pending'), 'domain': [('document_status', '=', 'sent')]},
+            'sign': {'label': _('Signed'), 'domain': [('document_status', 'in', ('sign', 'partially_sign'))]},
+        }
+    
     
     @http.route('/my/service_agreements', type='http', methods=['GET'], auth='user', website=True, sitemap=False)
-    def my_service_agreements(self, **kwargs):
+    def my_service_agreements(self, sortby=None, filterby=None, **kwargs):
         
         if not self.user_has_sa():
             return request.redirect('/my/home')
         
         request.session.pop('error_sa_partner', None)
         
-        partner_id = request.env.user.partner_id
+        searchbar_sortings = self._get_sa_searchbar_sortings()
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
         
-        service_agreements = request.env['pao.sign.sa.agreements.sent'].sudo().search([
+        searchbar_filters = self._get_sa_searchbar_filters()
+        # default filter by value
+        if not filterby:
+            filterby = 'all'
+        domain = searchbar_filters[filterby]['domain']
+        
+        partner_id = request.env.user.partner_id
+        domain += [
             ('signer_id', 'in', [partner_id.id] + partner_id.child_ids.ids),
             ('document_status', 'not in', ('cancel', 'exception'))
-        ])
+        ]
         
-        return request.render('pao_client_sa_inquiry.my_service_agreements_view', {'service_agreements': service_agreements, 'page_name': 'partner_sa_list'})
+        service_agreements = request.env['pao.sign.sa.agreements.sent'].sudo().search(domain, order=order)
+        
+        return request.render('pao_client_sa_inquiry.my_service_agreements_view', {
+            'service_agreements': service_agreements, 
+            'page_name': 'partner_sa_list', 
+            'searchbar_sortings': searchbar_sortings, 
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+        })
     
     
     @http.route('/my_service_agreements/download/attachments/<int:id>', type='http', auth="user", website=True)
