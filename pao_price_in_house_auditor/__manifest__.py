52277def7b893cc5042af5213fe69af10e455740
Samuel <scastro@pao-mx.com>
2024-09-27 09:24:33 -0700

    test

---
+++ pao_price_in_house_auditor/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PAO Proce In House Auditor',
+    'version': '17.0.0.1.0',
+    'author': 'samuel castro',
+    'category': '',
+    'website': 'https://paomx.com',
+    'summary': """
+        The purpose of this module is to have a purchase price for in house auditor
+    """,
+    'description': """
+    
+    """,
+    'depends': ['base','purchase','product'
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+    ],
+    'license': 'LGPL-3',
+}
---
+++ pao_price_in_house_auditor/models/product_supplier_info.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+
+class ProductSupplierInfor(models.Model):
+    _inherit='product.supplierinfo'
+
+    pao_price_in_house_auditor = fields.Float(string="Price For In House Auditor")
+
+    
+    
