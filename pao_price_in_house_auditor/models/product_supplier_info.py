36fc6649b918822e1fb379428e1c58a465e08004
Samuel <scastro@pao-mx.com>
2024-09-27 10:02:30 -0700

    test

--- pao_price_in_house_auditor/models/product_supplier_info.py
+++ pao_price_in_house_auditor/models/product_supplier_info.py
@@ -3,10 +3,9 @@ from odoo import fields, models, api
 class ProductSupplierInfor(models.Model):
     _inherit='product.supplierinfo'
 
-    def _get_in_house_auditor(self):
-        return self.partner_id.is_an_in_house_auditor 
+    pao_is_an_in_house_auditor = fields.Boolean(realated="partner_id.is_an_in_house_auditor", string="Is an in house auditor")
 
-    pao_price_in_house_auditor = fields.Float(string="Price For In House Auditor", readonly=_get_in_house_auditor, default=0.00,)
+    pao_price_in_house_auditor = fields.Float(string="Price For In House Auditor", default=0.00,)
 
     
     
