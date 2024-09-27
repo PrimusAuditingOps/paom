be900f7c442e992ca2ca5a3092d8534474b88c23
Samuel <scastro@pao-mx.com>
2024-09-27 10:50:49 -0700

    test

--- pao_price_in_house_auditor/models/product_supplier_info.py
+++ pao_price_in_house_auditor/models/product_supplier_info.py
@@ -7,20 +7,13 @@ _logger = getLogger(__name__)
 class ProductSupplierInfor(models.Model):
     _inherit='product.supplierinfo'
 
-    pao_is_an_in_house_auditor = fields.Boolean(realated="partner_id.is_an_in_house_auditor", string="Is an in house auditor")
-
     pao_price_in_house_auditor = fields.Float(string="Price For In House Auditor", default=0.00,)
 
 
     @api.onchange('pao_price_in_house_auditor')
     def _change_pao_price_in_house_auditor(self):
         for rec in self:
-            _logger.error("asads")
-            _logger.error(rec.pao_is_an_in_house_auditor)
-            _logger.error(rec.partner_id)
-            _logger.error(rec.partner_id.name)
-            _logger.error(rec.partner_id.is_an_in_house_auditor)
-            if not rec.pao_is_an_in_house_auditor:
+            if not rec.partner_id.is_an_in_house_auditor:
                 rec.pao_price_in_house_auditor = 0.00
 
 
