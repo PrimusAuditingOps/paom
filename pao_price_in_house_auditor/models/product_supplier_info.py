47cafc0a1f5be4102fe052bfbd809c1dbd63dae7
Samuel <scastro@pao-mx.com>
2024-09-27 10:46:01 -0700

    test

--- pao_price_in_house_auditor/models/product_supplier_info.py
+++ pao_price_in_house_auditor/models/product_supplier_info.py
@@ -1,4 +1,8 @@
 from odoo import fields, models, api
+from logging import getLogger
+
+
+_logger = getLogger(__name__)
 
 class ProductSupplierInfor(models.Model):
     _inherit='product.supplierinfo'
@@ -11,6 +15,9 @@ class ProductSupplierInfor(models.Model):
     @api.onchange('pao_price_in_house_auditor')
     def _change_pao_price_in_house_auditor(self):
         for rec in self:
+            _logger.error("asads")
+            _logger.error(rec.pao_is_an_in_house_auditor)
+            _logger.error(rec.partner_id)
             if not rec.pao_is_an_in_house_auditor:
                 rec.pao_price_in_house_auditor = 0.00
 
