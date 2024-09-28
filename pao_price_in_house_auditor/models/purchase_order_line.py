40b70b96e856115043db5b8383396d0912efb35b
Samuel <scastro@pao-mx.com>
2024-09-27 18:02:21 -0700

    test

--- pao_price_in_house_auditor/models/purchase_order_line.py
+++ pao_price_in_house_auditor/models/purchase_order_line.py
@@ -17,19 +17,19 @@ class PurchaseOrderLine(models.Model):
     def _onchange_product_id_in_house_auditor(self):
         for rec in self:
             _logger.error(rec.product_id._origin.id)
-            if not rec._origin.product_id:
+            if not rec.product_id._origin:
                 rec.pao_price_in_house_auditor = 0.00
                 return
             else:
                 params = rec._get_select_sellers_params()
-                seller = rec._origin.product_id._select_seller(
+                seller = rec.product_id._origin._select_seller(
                     partner_id=rec.partner_id,
                     quantity=rec.product_qty,
                     date=rec.order_id.date_order and rec.order_id.date_order.date() or fields.Date.context_today(rec),
                     uom_id=rec.product_uom,
                     params=params)
                 if seller:
-                    price_unit = rec.env['account.tax']._fix_tax_included_price_company(seller.pao_price_in_house_auditor, rec._origin.product_id.supplier_taxes_id, rec.taxes_id, rec.company_id) if seller else 0.0
+                    price_unit = rec.env['account.tax']._fix_tax_included_price_company(seller.pao_price_in_house_auditor, rec.product_id._origin.supplier_taxes_id, rec.taxes_id, rec.company_id) if seller else 0.0
                     price_unit = seller.currency_id._convert(price_unit, rec.currency_id, rec.company_id, rec.date_order or fields.Date.context_today(rec), False)
                     price_unit = float_round(price_unit, precision_digits=max(rec.currency_id.decimal_places, self.env['decimal.precision'].precision_get('Product Price')))
                     rec.pao_price_in_house_auditor = seller.product_uom._compute_price(price_unit, rec.product_uom)
