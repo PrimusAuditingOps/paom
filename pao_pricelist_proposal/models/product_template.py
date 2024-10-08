d3863900f7e15dea781c467ccbf970beae39afcb
Uzueta <uzueta.m@hotmail.com>
2024-10-08 08:55:56 -0700

    test

--- pao_pricelist_proposal/models/product_template.py
+++ pao_pricelist_proposal/models/product_template.py
@@ -1,5 +1,7 @@
-from odoo import models, api, _
-from odoo.exceptions import UserError
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from logging import getLogger
+_logger = getLogger(__name__)
 
 class ProductTemplateInherit(models.Model):
 
@@ -13,11 +15,25 @@ class ProductTemplateInherit(models.Model):
         # Call the super method with the updated default values
         return super(ProductTemplateInherit, self).copy(default)
     
-    @api.constrains("name")
-    def _check_existing_record_translations(self):
-        if self._origin:  # Check if the record is an existing one
-            default_lang = self.env.lang
-            for lang in self.env.langs:
-                if lang.code != default_lang:
-                    if not self.with_context(lang=lang.code).name:
-                        raise UserError("Translations for 'Name' are missing in language '%s'. Please set them before updating the record." % lang.code)
+    @api.model
+    def create(self, vals):
+        self._check_name_translations(vals)
+        return super(ProductTemplateInherit, self).create(vals)
+
+    def write(self, vals):
+        self._check_name_translations(vals)
+        return super(ProductTemplateInherit, self).write(vals)
+
+    def _check_name_translations(self, vals):
+        # Check if the name is set in each active language
+        if 'name' in vals and not vals['name']:
+            raise ValidationError("Please set a name before saving the product.")
+
+        # Ensure translations are also set
+        for lang in self.env['res.lang'].search([]).mapped('code'):
+            translated_name = self.with_context(lang=lang).name_get()[0][1]
+            _logger.warning(self.with_context(lang=lang).name_get())
+            if not translated_name.strip():
+                raise ValidationError(
+                    f"Please set a translated name for the language: {lang}"
+                )
