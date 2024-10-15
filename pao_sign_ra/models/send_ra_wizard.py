abb1bb4cf49704d4e7fcf0cfc2973e0a3abde30b
Manuel Uzueta <muzueta@pao-mx.com>
2024-10-15 12:15:16 -0700

    test

--- pao_sign_ra/models/send_ra_wizard.py
+++ pao_sign_ra/models/send_ra_wizard.py
@@ -17,51 +17,50 @@ class SendRaWizard(models.Model):
     pao_registration_numbers_ids = fields.Many2many(
         comodel_name='servicereferralagreement.registrynumber',
         string='Registration Numbers',
-        # domain=lambda self: self.get_domain(),
         required=True
     )
     
+    filtered_registration_numbers = fields.Many2many(
+        'servicereferralagreement.registrynumber',
+        'filtered_registration_numbers_registry_number_rel',
+        string='Registration Numbers',
+    )
     @api.model
     def default_get(self, fields):
         res = super(SendRaWizard, self).default_get(fields)
         
-        self.set_domain()
+        # Get the purchase_order_id from the context or any other source
+        purchase_order_id = self.env.context.get('default_purchase_order_id')
+        _logger.warning(purchase_order_id)
+        if purchase_order_id:
+            purchase_order = self.env['purchase.order'].browse(int(purchase_order_id))
+            listnumbers = []
 
-        return res
-    
-    # @api.onchange('purchase_order_id')
-    def set_domain(self):
-        _logger.warning("changing")
-        po_id = self.env.context.get("default_purchase_order_id")
-        if po_id:
-            # Set the domain based on the value of field_a
-            return {
-                'domain': {
-                    'pao_registration_numbers_ids': [('id', 'in', self.get_domain(po_id))]
-                }
-            }
-        else:
-            # Clear the domain if field_a is not set
-            return {
-                'domain': {
-                    'pao_registration_numbers_ids': []
-                }
-            }
-    
-    @api.model
-    def get_domain(self, po_id):
-        listnumbers = []
-        _logger.warning(po_id)
-        po = self.env['purchase.order'].browse(int(po_id))
-        if po:
-            for line in po.order_line:
+            for line in purchase_order.order_line:
                 if line.registrynumber_id and line.registrynumber_id.id not in listnumbers:
                     listnumbers.append(line.registrynumber_id.id)
-        _logger.warning(listnumbers)
-        return listnumbers
-        # domain = [('id', 'in', listnumbers)]
-        # _logger.warning(domain)
-        # return domain
+
+            _logger.warning(listnumbers)  # Log the registration numbers
+            
+            # Set the domain for the Many2many field in the wizard context
+            res['filtered_registration_numbers'] = [(6, 0, listnumbers)]
+        
+        return res
+    
+    # @api.model
+    # def get_domain(self, po_id):
+    #     listnumbers = []
+    #     _logger.warning(po_id)
+    #     po = self.env['purchase.order'].browse(int(po_id))
+    #     if po:
+    #         for line in po.order_line:
+    #             if line.registrynumber_id and line.registrynumber_id.id not in listnumbers:
+    #                 listnumbers.append(line.registrynumber_id.id)
+    #     _logger.warning(listnumbers)
+    #     return listnumbers
+    #     # domain = [('id', 'in', listnumbers)]
+    #     # _logger.warning(domain)
+    #     # return domain
     
     attachment_ids = fields.Many2many(
         'ir.attachment', 'send_ra_wizard_ir_attachments_rel',
