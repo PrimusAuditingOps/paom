233d769ebf23b869ac5466e0ece0037d3502077c
Manuel Uzueta <muzueta@pao-mx.com>
2024-10-15 10:20:10 -0700

    test

--- pao_sign_ra/models/send_ra_wizard.py
+++ pao_sign_ra/models/send_ra_wizard.py
@@ -30,9 +30,9 @@ class SendRaWizard(models.Model):
     def get_domain(self):
         purchase_order_id = self.env.context.get('default_purchase_order_id')
         _logger.warning(purchase_order_id)
+        listnumbers = []
         if purchase_order_id:
             purchase_order = self.env['purchase.order'].browse(int(purchase_order_id))
-            listnumbers = []
 
             for line in purchase_order.order_line:
                 if line.registrynumber_id and line.registrynumber_id.id not in listnumbers:
