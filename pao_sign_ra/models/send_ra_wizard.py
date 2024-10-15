65b170951f1e77631f7b3496bf04346dad998f90
Manuel Uzueta <muzueta@pao-mx.com>
2024-10-15 10:21:30 -0700

    test

--- pao_sign_ra/models/send_ra_wizard.py
+++ pao_sign_ra/models/send_ra_wizard.py
@@ -61,25 +61,25 @@ class SendRaWizard(models.Model):
             
         super(SendRaWizard, self).action_send_mail()
         
-    @api.model
-    def default_get(self, fields):
-        res = super(SendRaWizard, self).default_get(fields)
+    # @api.model
+    # def default_get(self, fields):
+    #     res = super(SendRaWizard, self).default_get(fields)
         
-        # Get the purchase_order_id from the context or any other source
-        purchase_order_id = self.env.context.get('default_purchase_order_id')
-        _logger.warning(purchase_order_id)
-        if purchase_order_id:
-            purchase_order = self.env['purchase.order'].browse(int(purchase_order_id))
-            listnumbers = []
+    #     # Get the purchase_order_id from the context or any other source
+    #     purchase_order_id = self.env.context.get('default_purchase_order_id')
+    #     _logger.warning(purchase_order_id)
+    #     if purchase_order_id:
+    #         purchase_order = self.env['purchase.order'].browse(int(purchase_order_id))
+    #         listnumbers = []
 
-            for line in purchase_order.order_line:
-                if line.registrynumber_id and line.registrynumber_id.id not in listnumbers:
-                    listnumbers.append(line.registrynumber_id.id)
+    #         for line in purchase_order.order_line:
+    #             if line.registrynumber_id and line.registrynumber_id.id not in listnumbers:
+    #                 listnumbers.append(line.registrynumber_id.id)
 
-            _logger.warning(listnumbers)  # Log the registration numbers
+    #         _logger.warning(listnumbers)  # Log the registration numbers
             
-            # Set the domain for the Many2many field in the wizard context
-            res['filtered_registration_numbers'] = [(6, 0, listnumbers)]
+    #         # Set the domain for the Many2many field in the wizard context
+    #         res['filtered_registration_numbers'] = [(6, 0, listnumbers)]
         
-        return res
+    #     return res
     
