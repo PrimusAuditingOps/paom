b6eb6550228404e52cd26d909c91eb607af86aa6
Manuel Uzueta <muzueta@pao-mx.com>
2024-10-17 13:01:30 -0700

    test

--- pao_sign_ra/models/ra_mail_templates.py
+++ pao_sign_ra/models/ra_mail_templates.py
@@ -4,5 +4,6 @@ class AllowedTemplates(models.Model):
     _name = 'ra.mail.templates'
     _description = 'RA Allowed Email Templates'
 
+    sequence =  fields.Integer()
     template_id = fields.Many2one('mail.template', string="Template", required=True, domain=[('model', '=', 'purchase.order')])
 
