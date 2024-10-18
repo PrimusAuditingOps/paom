412739a31e27535278ddc93e21e573ded2fc0d18
Manuel Uzueta <muzueta@pao-mx.com>
2024-10-18 08:52:43 -0700

    test

--- pao_sign_ra/controller/portal.py
+++ pao_sign_ra/controller/portal.py
@@ -66,7 +66,7 @@ class SignRAPortal(portal.CustomerPortal):
         
         return request.redirect('/ra_request/sign/'+str(id)+'/'+str(token))
     
-    @http.route('/ra_request/submit_sign/<int:id>/<string:token>', type='http', auth='public', website=True)
+    @http.route('/ra_request/submit_sign/<int:id>/<string:token>', type='json', auth='public', website=True)
     def ra_sign_submit_portal(self, id, token):
         try:
             ra_document = self._document_check_access('ra.document', id, access_token=token)
@@ -75,31 +75,25 @@ class SignRAPortal(portal.CustomerPortal):
         
         ra_document.write({'status': 'sign'})
         return request.render('pao_sign_ra.process_complete_ra_request_portal_view', {'ra_status': ra_document.status})
+    
+    # @http.route(['/pricelist_proposal/<int:id>/<string:token>/accept'], type='json', auth="public", website=True)
+    # def proposal_portal_accept(self, id, token, signature):
+    #     if not signature:
+    #         redirect_link = '/pricelist_proposal/%s/%s' % (id, token)
+    #         return request.redirect(redirect_link)
         
-    # @http.route('/sign_ra/<string:token>/<string:idresponse>', type='http', auth="public", website=True)
-    # def action_confirm_audit(self, token, idresponse, **kwargs):
+    #     try:
+    #         proposal_sudo = self._document_check_access('pao.pricelist.proposal', id, access_token=token)
+    #     except (AccessError, MissingError):
+    #         return request.redirect('/')
         
-    #     purchaseconfirmation = request.env['auditconfirmation.purchaseconfirmation'].sudo().search(['&',('ac_access_token', '=', token),'|',('ac_consumed','=',False),('ac_audit_confirmation_status','=', '3')])
-    #     if not purchaseconfirmation or idresponse not in ('1','2','3'): 
-    #         return request.not_found()
-    #     purchase =  request.env['purchase.order'].sudo().search([('id', '=', purchaseconfirmation.ac_id_purchase)])
+    #     proposal_sudo.signature = signature
+    #     accept_proposal = proposal_sudo.accept_proposal_action()
         
-    #     lang = purchase.partner_id.lang or get_lang(request.env).code
-    #     access_token = purchase._portal_ensure_token()
-    #     if idresponse == '1':
-    #         if purchase.ac_request_travel_expenses and not purchaseconfirmation.ac_consumed_travel_expenses:   
-    #             pageredirect = "/response/travel/expenses?access_token="+access_token+"&number="+str(purchase.id)+"&ra_verification=1"
-    #             return request.redirect(pageredirect)
-    #         else:
-    #             return request.env['ir.ui.view'].with_context(lang=lang)._render_template('auditconfirmation.external_auditor_sign_portal_view', 
-    #             {
-    #                 'signer': purchase.partner_id.name,
-    #                 'urlAccept': purchase.get_portal_url(suffix='/accept/audit'),
-    #                 "serviceagreement": purchaseconfirmation,
-    #                 "documents": documents
-    #             })
-    #     else:
-    #         return request.env['ir.ui.view'].with_context(lang=lang)._render_template('auditconfirmation.audit_rejected_external_page_view', 
-    #         {
-    #             'urlrejected': purchase.get_portal_url(suffix='/decline/audit'),
-    #         })
+    #     if accept_proposal:
+    #         base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+    #         redirect_link = url_join(base_url, '/pricelist_proposal/%s/%s' % (id, token))
+    #         return {
+    #             'force_refresh': True,
+    #             'redirect_url':  redirect_link
+    #         }
