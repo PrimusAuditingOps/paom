38ddb13a230d12aac8a618432665e2d7a4803fac
Samuel <scastro@pao-mx.com>
2024-12-10 23:34:22 -0700

    [MOD] pao_sales_order_service_counter

---
+++ pao_sales_order_service_counter/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PAO Sales Order Service Counter',
+    'version': '17.0.0.1.0',
+    'author': 'samuel castro',
+    'category': '',
+    'website': 'https://paomx.com',
+    'summary': """
+        The purpose of this module is to count the service in the order line.
+    """,
+    'description': """
+    
+    """,
+    'depends': ['base','sale'
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/sale_order.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ pao_sales_order_service_counter/models/sale_order.py
@@ -0,0 +1,11 @@
+from odoo import fields, models, api
+
+class SaleOrder(models.Model):
+    _inherit='sale.order'
+
+    pao_service_counter = fields.Integer(string="Total Service Lines", compute='_pao_compute_service_counter',store=True)
+    
+    @api.depends("order_line")
+    def _pao_compute_service_counter(self):
+        for rec in self:
+            rec.pao_service_counter = len(rec.order_line.filtered(lambda ol: ol.product_id))
