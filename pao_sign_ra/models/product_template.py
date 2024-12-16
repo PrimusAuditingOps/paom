ee48ca0f04861004461e784743456766a9a8c484
Uzueta <uzueta.m@hotmail.com>
2024-12-15 21:49:30 -0700

    [MOD] pao_sign_ra

--- pao_sign_ra/models/product_template.py
+++ pao_sign_ra/models/product_template.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
-class ProductProductInherit(models.Model):
-    _inherit = 'product.product'
+class ProductTemplateInherit(models.Model):
+    _inherit = 'product.template'
 
     is_travel_expenses = fields.Boolean(default=False, string="It's a travel expense")
 
