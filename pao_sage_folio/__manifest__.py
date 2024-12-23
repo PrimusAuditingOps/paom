3fc64abd262b60ae113d0a47161bdac96529c81d
Samuel <scastro@pao-mx.com>
2024-12-23 13:38:40 -0700

    [MOD] pao_sign_sa and servicereferralagreement

---
+++ pao_sage_folio/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PAO Sage Folio',
+    'version': '17.0.0.1.0',
+    'author': 'samuel castro',
+    'category': '',
+    'website': 'https://paomx.com',
+    'summary': """
+        The purpose of this module is to a SAGE folio for USA
+    """,
+    'description': """
+        The purpose of this module is to a SAGE folio for USA.
+    """,
+    'depends': ['base','account'
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/account_move_form_view.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ pao_sage_folio/models/account_move.py
@@ -0,0 +1,10 @@
+from odoo import api, models, fields
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountMoveInherit(models.Model):
+    _inherit = 'account.move'
+
+    pao_sage_folio_usa = fields.Char(string="SAGE Reference")
+    
