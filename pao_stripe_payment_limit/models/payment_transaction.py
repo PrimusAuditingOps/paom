622e541ce4c7be30e97710755ffc85a928dbfa88
Uzueta <uzueta.m@hotmail.com>
2025-05-27 13:02:30 -0700

    test

--- pao_stripe_payment_limit/models/payment_transaction.py
+++ pao_stripe_payment_limit/models/payment_transaction.py
@@ -4,12 +4,70 @@ from logging import getLogger
 _logger = getLogger(__name__)
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
-    
-    def _create_payment_vals(self, invoice):
-            vals = super()._create_payment_vals(invoice)
-            _logger.warning("ENTRA*******************")
+
+    def _create_payment(self, **extra_create_values):
+            """Create an `account.payment` record for the current transaction.
+
+            If the transaction is linked to some invoices, their reconciliation is done automatically.
+
+            Note: self.ensure_one()
+
+            :param dict extra_create_values: Optional extra create values
+            :return: The created payment
+            :rtype: recordset of `account.payment`
+            """
+            self.ensure_one()
+            
+            _logger.warning("ENTRA****")
+            
             _logger.warning(self.provider_id.code)
+            
+            _logger.warning("****************")
+
             if self.provider_id.code == 'stripe':
-                custom_ref = (f'{self.reference}')
-                vals['ref'] = custom_ref
-            return vals
+                reference = (f'{self.reference}')
+            else:
+                reference = (f'{self.reference} - '
+                            f'{self.partner_id.display_name or ""} - '
+                            f'{self.provider_reference or ""}'
+                            )
+                
+            _logger.warning(reference)
+
+
+            payment_method_line = self.provider_id.journal_id.inbound_payment_method_line_ids\
+                .filtered(lambda l: l.payment_provider_id == self.provider_id)
+            payment_values = {
+                'amount': abs(self.amount),  # A tx may have a negative amount, but a payment must >= 0
+                'payment_type': 'inbound' if self.amount > 0 else 'outbound',
+                'currency_id': self.currency_id.id,
+                'partner_id': self.partner_id.commercial_partner_id.id,
+                'partner_type': 'customer',
+                'journal_id': self.provider_id.journal_id.id,
+                'company_id': self.provider_id.company_id.id,
+                'payment_method_line_id': payment_method_line.id,
+                'payment_token_id': self.token_id.id,
+                'payment_transaction_id': self.id,
+                'ref': reference,
+                **extra_create_values,
+            }
+            payment = self.env['account.payment'].create(payment_values)
+            payment.action_post()
+
+            # Track the payment to make a one2one.
+            self.payment_id = payment
+
+            # Reconcile the payment with the source transaction's invoices in case of a partial capture.
+            if self.operation == self.source_transaction_id.operation:
+                invoices = self.source_transaction_id.invoice_ids
+            else:
+                invoices = self.invoice_ids
+            if invoices:
+                invoices.filtered(lambda inv: inv.state == 'draft').action_post()
+
+                (payment.line_ids + invoices.line_ids).filtered(
+                    lambda line: line.account_id == payment.destination_account_id
+                    and not line.reconciled
+                ).reconcile()
+
+            return payment
