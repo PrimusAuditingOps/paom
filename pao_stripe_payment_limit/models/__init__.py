1c4ccfcf4789b8aa73e73b23ccec3ab715c29a32
Uzueta <uzueta.m@hotmail.com>
2025-05-27 13:24:44 -0700

    test

--- pao_stripe_payment_limit/models/__init__.py
+++ pao_stripe_payment_limit/models/__init__.py
@@ -1,2 +1,2 @@
 from . import res_partner
-from . import payment_transaction
+from . import account_payment
--- pao_stripe_payment_limit/models/account_payment.py
+++ pao_stripe_payment_limit/models/account_payment.py
@@ -1,13 +1,10 @@
 from odoo import models, api
-from logging import getLogger
-_logger = getLogger(__name__)
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     @api.model
     def create(self, vals):
-        _logger.warning("ENTRAAAAAAAA********************")
         if vals.get('payment_transaction_id'):
             transaction = self.env['payment.transaction'].browse(vals['payment_transaction_id'])
             if transaction.provider_id.code == 'stripe':
