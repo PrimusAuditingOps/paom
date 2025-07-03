23abc6b0128a78b81c97ed0dc41bf9707fc0b080
Uzueta <uzueta.m@hotmail.com>
2025-07-03 12:22:42 -0700

    Se agrega logica para bank statement pdf 2.3

--- pao_expenses_portal/models/upload_statement.py
+++ pao_expenses_portal/models/upload_statement.py
@@ -116,6 +116,7 @@ class UploadExpenseStatement(models.TransientModel):
             lines = text.split('\n')
             for line in lines:
                 _logger.warning("*************EMPIEZA READER LINES*****************")
+                _logger.warning(line)
                 match = re.match(r"(?P<date>\d{2}/\d{2}/\d{4})\s+US\$(?P<amount>\d+\.\d{2})\s+US\$[\d\.]*\s+(?P<merchant>.+)", line)
                 if match:
                     _logger.warning("*************ENCUENTRA MATCH READER LINE*****************")
