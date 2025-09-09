5b4105dbda85f82e904deabb67d3a8dd4549f07c
samuel castro <samuel.castro.gonzalez@gmail.com>
2025-09-09 12:34:26 -0700

    [MOD] pao_invoice_report

--- pao_invoice_report/models/invoice_report.py
+++ pao_invoice_report/models/invoice_report.py
@@ -78,10 +78,9 @@ class InvoiceReport(models.Model):
     def _get_auditor(self):
         for rec in self:
             rec.auditor = False
-            for line in rec.move_line_id.sale_line_ids:
-                purchase_line = rec.env['purchase.order.line'].search([('sra_sale_line_ids', 'in', [line.id])])
-                for pline in purchase_line:
-                    rec.auditor = pline.partner_id.id
+            for purchase in rec.sale_order.purchase_order_id:
+                if purchase.state != 'cancel':
+                    rec.auditor = purchase.partner_id.id
 
     def _get_rate(self):
         for rec in self:
