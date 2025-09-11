f08040ca56783904772a87c3d869b16e7f82b369
Uzueta <uzueta.m@hotmail.com>
2025-09-11 13:37:35 -0700

    test

--- pao_sign_ra/models/ra_document.py
+++ pao_sign_ra/models/ra_document.py
@@ -131,6 +131,9 @@ class RADocument(models.Model):
                 'target': 'current', 
             }
             
+    def action_test_reminder(self):
+        self._send_ra_reminders()
+            
     @api.model
     def _send_ra_reminders(self):
         today = fields.Date.today()
