92b1944cb38f883a8a96a76d15779408f0356e19
Uzueta <uzueta.m@hotmail.com>
2025-09-11 15:37:03 -0700

    test

--- pao_sign_ra/models/ra_document.py
+++ pao_sign_ra/models/ra_document.py
@@ -158,8 +158,8 @@ class RADocument(models.Model):
                     'request_travel_expenses': rec.request_travel_expenses,
                     'template_id': rec.ra_template_id.id,
                     'composition_mode': 'comment',
-                    'model': 'ra.document',
-                    'res_ids': rec.ids,
+                    'model': 'purchase.order',
+                    'res_ids': rec.purchase_order_id.ids,
                 })
                 wizard.action_send_mail()
                 
