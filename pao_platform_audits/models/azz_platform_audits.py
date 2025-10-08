919744cabe37e718eb06b1c9eba8bb99169a6a21
samuel castro <samuel.castro.gonzalez@gmail.com>
2025-10-08 11:03:18 -0700

    test

--- pao_platform_audits/models/azz_platform_audits.py
+++ pao_platform_audits/models/azz_platform_audits.py
@@ -13,7 +13,6 @@ class PaoAzzPlatformAudits(models.Model):
         string= "Audit Date",
     )
     finished_date = fields.Date(
-        required=True,
         string= "Finished Date",
     )
     audit_id = fields.Integer(
@@ -29,11 +28,9 @@ class PaoAzzPlatformAudits(models.Model):
         string= "App ID",
     )
     is_announced = fields.Char(
-        required=True,
         string= "Is Announced",
     )
     pre_assessment = fields.Char(
-        required=True,
         string= "Pre-assessment",
     )
     audit_template = fields.Char(
@@ -49,15 +46,12 @@ class PaoAzzPlatformAudits(models.Model):
         string= "Organization",
     )
     organization_contact_name = fields.Char(
-        required=True,
         string= "Organization Contact Name",
     )
     organization_contact_email = fields.Char(
-        required=True,
         string= "Organization Contact Email",
     )
     entities = fields.Char(
-        required=True,
         string= "Entities",
     )
     auditor = fields.Char(
@@ -65,39 +59,30 @@ class PaoAzzPlatformAudits(models.Model):
         string= "Auditor",
     )
     coordinator = fields.Char(
-        required=True,
         string= "Coordinator",
     )
     plc = fields.Char(
-        required=True,
         string= "PLC",
     )
     cycle = fields.Char(
-        required=True,
         string= "Cycle",
     )
     country = fields.Char(
-        required=True,
         string= "Country",
     )
     state = fields.Char(
-        required=True,
         string= "State",
     )
     city = fields.Char(
-        required=True,
         string= "City",
     )
     certification_decision_date = fields.Date(
-        required=True,
         string= "Certification Decision Date",
     )
     commodities = fields.Char(
-        required=True,
         string= "Commodities",
     )
     shipper = fields.Char(
-        required=True,
         string= "Shipper",
     )
 
