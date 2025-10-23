509c4e0e08b06825f0a41c71f345b6d421d2184a
samuel castro <samuel.castro.gonzalez@gmail.com>
2025-10-22 21:50:35 -0700

    test

--- pao_platform_audits/models/sales_report.py
+++ pao_platform_audits/models/sales_report.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    pao_platform_audit_ids = fields.Many2many(
+    pao_platform_audit_ids = fields.One2many(
         related="sale_order_line_id.pao_platform_audit_ids"
     )
                     
