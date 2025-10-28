57788af54cec9a606edcb3610d077bc07a898875
samuel castro <samuel.castro.gonzalez@gmail.com>
2025-10-27 17:07:20 -0700

    test

--- pao_platform_audits/models/azz_platform_audits.py
+++ pao_platform_audits/models/azz_platform_audits.py
@@ -414,7 +414,7 @@ class PaoAzzPlatformAudits(models.Model):
     @api.depends('organization')
     def _compute_organization(self):
         for rec in self:
-            _logger.error(rec.env.company.id)
+            _logger.error(rec.company_id.id)
             rec.organization_id = None
             domain = [("name","=",rec.organization),("company_id","=",rec.env.company.id)]
             organization = self.env["servicereferralagreement.organization"].search(domain)
