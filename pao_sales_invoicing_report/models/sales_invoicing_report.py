2b146b577aabd3aa7c416035a67f69652dfde7ca
Uzueta <uzueta.m@hotmail.com>
2025-10-29 14:04:14 -0700

    test

--- pao_sales_invoicing_report/models/sales_invoicing_report.py
+++ pao_sales_invoicing_report/models/sales_invoicing_report.py
@@ -87,12 +87,13 @@ class SalesInvoicingReport(models.Model):
                     FROM account_move_line orig_l
                     JOIN account_move orig_a ON orig_l.move_id = orig_a.id
                     JOIN product_product orig_p ON orig_l.product_id = orig_p.id
+                    JOIN product_template orig_t ON orig_p.product_tmpl_id = orig_t.id
                     WHERE orig_a.id = a.reversed_entry_id
                         AND orig_a.currency_id = a.currency_id
                         AND (
-                            l.name ILIKE CONCAT('%[', orig_p.default_code, ']%') OR
-                            TRIM(l.name) ILIKE TRIM(orig_p.name)
-                            )
+                            l.name ILIKE CONCAT('%[', orig_p.default_code, ']%')
+                            OR TRIM(l.name) ILIKE TRIM(orig_t.name)
+                        )
                     LIMIT 1
                 ), l.product_id)
                 ELSE l.product_id
