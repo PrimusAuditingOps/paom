f5b10fd8e9a360cf40e88d9775f1b2dfa733e8c6
Uzueta <uzueta.m@hotmail.com>
2025-10-29 14:21:59 -0700

    test

--- pao_sales_invoicing_report/models/sales_invoicing_report.py
+++ pao_sales_invoicing_report/models/sales_invoicing_report.py
@@ -89,10 +89,7 @@ class SalesInvoicingReport(models.Model):
                     JOIN product_product orig_p ON orig_l.product_id = orig_p.id
                     WHERE orig_a.id = a.reversed_entry_id
                         AND orig_a.currency_id = a.currency_id
-                        AND (
-                            l.name ILIKE CONCAT('%[', orig_p.default_code, ']%') 
-                            OR l.name::text ILIKE REGEXP_REPLACE(orig_l.name::text, '^\[[^\]]*\]\s*', '')
-                        )
+                        AND l.name ILIKE CONCAT('%[', orig_p.default_code, ']%')
                     LIMIT 1
                 ), l.product_id)
                 ELSE l.product_id
