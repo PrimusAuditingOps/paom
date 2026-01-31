204bdba8d80f7340684abb5735775a965da54049
Uzueta <uzueta.m@hotmail.com>
2026-01-30 22:47:31 -0700

    test

--- pao_projects_marketing/__manifest__.py
+++ pao_projects_marketing/__manifest__.py
@@ -7,6 +7,7 @@
     "depends": ["base", "project"],
     'data': [
         'views/project_task.xml',
+        'views/requirement_type_views.xml',
     ],
     'license': 'LGPL-3',
 }
--- pao_projects_marketing/models/__init__.py
+++ pao_projects_marketing/models/__init__.py
@@ -1,1 +1,2 @@
 from . import project_task
+from . import pao_requirement_types_project
