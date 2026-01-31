d152c8c1e884483a20d819b4113199da3f0b49d9
Uzueta <uzueta.m@hotmail.com>
2026-01-30 23:01:17 -0700

    test

--- pao_projects_marketing/models/project_task.py
+++ pao_projects_marketing/models/project_task.py
@@ -21,17 +21,17 @@ class ProjectTask(models.Model):
         tracking=True,
     )
     
-    # pao_requirement = fields.Selection(
-    #     selection=[
-    #         ('0', "Electronic signatures / Business cards."),
-    #         ('1', "Upload documents to the website."),
-    #         ('2', "Memos for satellite offices."),
-    #         ('3', "Updates to existing designs"),
-    #         ('4', "New designs (banners, flyers, etc.)"),
-    #         ('5', "Souvenir orders - USA"),
-    #         ('9', "Other"),
-    #     ],
-    #     string="Requirement",
-    #     tracking=True,
-    # )
+    pao_requirement = fields.Selection(
+        selection=[
+            ('0', "Electronic signatures / Business cards."),
+            ('1', "Upload documents to the website."),
+            ('2', "Memos for satellite offices."),
+            ('3', "Updates to existing designs"),
+            ('4', "New designs (banners, flyers, etc.)"),
+            ('5', "Souvenir orders - USA"),
+            ('9', "Other"),
+        ],
+        string="Requirement",
+        tracking=True,
+    )
     
