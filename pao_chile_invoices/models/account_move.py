c4ffc34774c7a2bb15030b4454fa9ea76e6780eb
Uzueta <uzueta.m@hotmail.com>
2026-02-09 10:52:32 -0700

    test

--- pao_chile_invoices/models/account_move.py
+++ pao_chile_invoices/models/account_move.py
@@ -1,22 +1,40 @@
 from odoo import models, fields, api, _
 
-class ProjectTask(models.Model):
+class AccountMoveInherit(models.Model):
 
     _inherit='account.move'
     
-    auto_exchange_rate_lines_value = fields.Float(string="Automatic Exchange Rate")
+    auto_exchange_rate_lines_value = fields.Float(string="Automatic Exchange Rate", copy=False)
     
-    def update_exchange_rate_lines_action(self):
+    exchange_rate_applied = fields.Boolean(default=False, copy=False)
+    
+    def apply_exchange_rate_lines_action(self):
         for move in self:
             rate = move.auto_exchange_rate_lines_value
 
-            if move.state != 'draft' or not rate:
+            if move.state != 'draft' or not rate or move.exchange_rate_applied:
                 continue
 
             for line in move.invoice_line_ids:
                 if line.price_unit:
                     new_price = line.price_unit * rate
                     line.price_unit = new_price
+                    
+            move.exchange_rate_applied = True
+    
+    def undo_exchange_rate_lines_action(self):
+        for move in self:
+            rate = move.auto_exchange_rate_lines_value
+
+            if move.state != 'draft' or not rate or not move.exchange_rate_applied:
+                continue
+
+            for line in move.invoice_line_ids:
+                if line.price_unit:
+                    new_price = line.price_unit / rate
+                    line.price_unit = new_price
+                    
+            move.exchange_rate_applied = False
     
     # def _check_partner_id_has_the_same_company(self):
     #     for rec in self:
