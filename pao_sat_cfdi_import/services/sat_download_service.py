73daadecf8cb91b98f30a9a1b566b112b0c169db
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-02-23 18:04:13 -0700

    test

--- pao_sat_cfdi_import/services/sat_download_service.py
+++ pao_sat_cfdi_import/services/sat_download_service.py
@@ -114,7 +114,9 @@ class SATDownloadService(models.AbstractModel):
         timestamp = etree.SubElement(
             security,
             '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp',
-            '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id': '_0'
+            {
+                '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id': '_0'
+            }
         )
 
         etree.SubElement(timestamp,
