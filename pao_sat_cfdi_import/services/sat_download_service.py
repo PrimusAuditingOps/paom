e30748472d6ae26c87e7cd418005c81d2f0a3a39
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-02-24 10:12:48 -0700

    test

--- pao_sat_cfdi_import/services/sat_download_service.py
+++ pao_sat_cfdi_import/services/sat_download_service.py
@@ -125,7 +125,6 @@ class SATDownloadService(models.AbstractModel):
             '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}Security',
             {
                 '{http://schemas.xmlsoap.org/soap/envelope/}mustUnderstand': '1',
-                '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id': 'SECURITY-1'
             }
         )
 
