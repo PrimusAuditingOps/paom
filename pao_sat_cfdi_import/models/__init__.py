91718d1df74836277e5495d814079b4ad18d0511
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-02-27 16:32:48 -0700

    test

--- pao_sat_cfdi_import/models/__init__.py
+++ pao_sat_cfdi_import/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from . import pao_sat_cfdi_packages
 from . import pao_sat_cfdi_request
 from . import pao_fiel_certificate
 from . import res_company
-from . import pao_sat_cfdi_packages
