6afc93856e538a81f1962acfe7b909ac2aba062d
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-03-17 15:37:06 -0700

    test

--- pao_sat_cfdi_import/models/pao_sat_cfdi_xml.py
+++ pao_sat_cfdi_import/models/pao_sat_cfdi_xml.py
@@ -72,6 +72,32 @@ class PAOSatCFDIXml(models.Model):
         string='SAT CDFI request',
         ondelete='cascade'
     )
+    method_of_payment = fields.Selection(
+        [
+            ('01', 'Efectivo'),
+            ('02', 'Cheque nominativo'),
+            ('03', 'Transferencia electrónica de fondos'),
+            ('04', 'Tarjeta de crédito'),
+            ('05', 'Monedero electrónico'),
+            ('06', 'Dinero electrónico'),
+            ('08', 'Vales de despensa'),
+            ('12', 'Dación en pago'),
+            ('13', 'Pago por subrogación'),
+            ('14', 'Pago por consignación'),
+            ('15', 'Condonación'),
+            ('17', 'Compensación'),
+            ('23', 'Novación'),
+            ('24', 'Confusión'),
+            ('25', 'Remisión de deuda'),
+            ('26', 'Prescripción o caducidad'),
+            ('27', 'A satisfacción del acreedor'),
+            ('28', 'Tarjeta de débito'),
+            ('29', 'Tarjeta de servicios'),
+            ('30', 'Aplicación de anticipos'),
+            ('99', 'Por definir'),
+        ],
+        string="Method of Payment"
+    )
 
     cfdi_use = fields.Selection(
         [
