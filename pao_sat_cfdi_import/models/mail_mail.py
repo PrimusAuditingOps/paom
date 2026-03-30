afc54a07bdd0c58a4a31ccec62def6ef3c44eae9
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-03-30 16:04:16 -0700

    test

--- pao_sat_cfdi_import/models/mail_mail.py
+++ pao_sat_cfdi_import/models/mail_mail.py
@@ -4,18 +4,15 @@ class MailMail(models.Model):
     _inherit = 'mail.mail'
 
     def _send(self, *args, **kwargs):
-        IrMailServer = self.env['ir.mail_server']
-
         for mail in self:
-            mail_server = IrMailServer._get_mail_server(mail)
+            mail_server = mail.mail_server_id
 
             if mail_server and mail_server.smtp_host:
-                if mail_server.name == 'Amazon SES Hector':
+                if 'amazonaws.com' in mail_server.smtp_host:
                     mail.email_from = 'notifications@pao-usa.com'
                     mail.reply_to = 'notifications@pao-usa.com'
                     mail.bounce_address = 'notifications@pao-usa.com'
 
         return super()._send(*args, **kwargs)
-
 
 
