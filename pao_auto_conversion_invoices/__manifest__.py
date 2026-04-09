7a19b1f67d049f107b1ff5c812c38acf0393db42
Uzueta <uzueta.m@hotmail.com>
2026-04-08 21:05:45 -0700

    [ADD] pao_auto_conversion_invoices

---
+++ pao_auto_conversion_invoices/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "PAO: Auto Conversion Invoices",
+    "version": "1.0",
+    "author": "Manuel Uzueta Gil",
+    "category": "",
+    "website": "https://paomx.com",
+    "depends": ["base", "account", "pao_chile_invoices"],
+    'data': [
+        'views/account_move_inherit_form.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ pao_auto_conversion_invoices/models/account_move.py
@@ -0,0 +1,90 @@
+from odoo import models, fields, api, _
+
+class AccountMoveInherit(models.Model):
+
+    _inherit='account.move'
+    
+    auto_conversion_applied = fields.Boolean(default=False, copy=False)
+    can_auto_convert = fields.Boolean(default=False, copy=False)
+    
+    def apply_auto_convert_to_mxn(self):
+        if not self.can_auto_convert or self.company_id.id != 1 or self.auto_conversion_applied or self.state != 'draft':
+            return
+
+        usd = self.env.ref('base.USD', raise_if_not_found=False)
+        mxn = self.env.ref('base.MXN', raise_if_not_found=False)
+
+        if not usd or not mxn:
+            return
+
+        date = self.invoice_date
+        rate = usd._get_conversion_rate(usd, mxn, self.company_id, date)
+        if not rate:
+            return {'warning': {
+                'title': _("Exchange Rate Not Available"),
+                'message': _("No exchange rate was found for the invoice date. Please make sure a valid exchange rate exists for the selected currencies before proceeding"),
+            }}
+            
+        for line in self.invoice_line_ids:
+            line.price_unit = usd._convert(
+                line.price_unit,
+                mxn,
+                self.company_id,
+                date,
+            )
+            line.exchange_rate_value = rate
+            line.exchange_rate_applied = True
+
+        self.auto_conversion_applied = True
+        self.can_auto_convert = False
+        
+    def undo_auto_convert_to_mxn(self):
+        if not self.auto_conversion_applied or self.state != 'draft':
+            return
+
+        for line in self.invoice_line_ids:
+            if line.exchange_rate_applied:
+                # Reverse: MXN / rate = USD
+                line.price_unit /= line.exchange_rate_value
+                line.exchange_rate_applied = False
+                line.exchange_rate_value = None
+                
+        self.auto_conversion_applied = False
+        
+        self.currency_id = self.env.ref('base.USD', raise_if_not_found=False)
+
+    @api.onchange('currency_id')
+    def _onchange_currency_id_activate_auto_convert(self):
+        if self.company_id.id != 1 or self.auto_conversion_applied or self.state != 'draft':
+            return
+        
+        self.can_auto_convert = False
+        
+        # Only trigger when changing from USD to MXN
+        usd = self.env.ref('base.USD', raise_if_not_found=False)
+        mxn = self.env.ref('base.MXN', raise_if_not_found=False)
+        
+        if not usd or not mxn:
+            return
+
+        origin = self._origin.currency_id  # currency before the change
+        
+        if origin == usd and self.currency_id == mxn:
+            self.can_auto_convert = True
+            return {
+                'warning': {
+                    'title': _('Price Conversion Available'),
+                    'message': _('The invoice currency has been changed from USD to MXN. Use the "Convert Prices" button to automatically apply the exchange rate to the invoice lines.'),
+                }
+            }
+            # return {
+            #     'type': 'ir.actions.client',
+            #     'tag': 'display_notification',
+            #     'params': {
+            #         'type': 'info',
+            #         'title': _('Price Conversion Available'),
+            #         'message': _('The invoice currency has been changed from USD to MXN. Use the "Convert Prices" button to automatically apply the exchange rate to the invoice lines.'),
+            #         'sticky': True,
+            #         'next': {'type': 'ir.actions.act_window_close'},
+            #     }
+            # }
