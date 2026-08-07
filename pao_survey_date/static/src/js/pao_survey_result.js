e60132bdbe71d2d8b9721464dfed97879b544398
samuel castro <samuel.castro.gonzalez@gmail.com>
2026-08-07 11:51:02 -0700

    test

--- pao_survey_date/static/src/js/pao_survey_result.js
+++ pao_survey_date/static/src/js/pao_survey_result.js
@@ -4,12 +4,19 @@ function ready() {
     document.getElementById("pao_search_date").addEventListener('click', search_date);
 
     pao_date = document.getElementById("pao_start_date");
+    pao_end_date = document.getElementById("pao_end_date");
     let params = new URLSearchParams(window.location.search);
     if (params.get('pao_date')){
         pao_date.value=params.get('pao_date');
     }
     else{
         pao_date.value = "";
+    }
+    if (params.get('pao_end_date')){
+        pao_end_date.value=params.get('pao_end_date');
+    }
+    else{
+        pao_end_date.value = "";
     }
 
 
@@ -19,11 +26,13 @@ function ready() {
 
             let params = new URLSearchParams(window.location.search);
             params.set('pao_date', date);
+            params.set('pao_end_date', date);
             window.location.href = window.location.pathname + '?' + params.toString();
         }
         else{
             let params = new URLSearchParams(window.location.search);
             params.delete('pao_date');
+            params.delete('pao_end_date');
             window.location.href = window.location.pathname + '?' + params.toString();
         }
     }
