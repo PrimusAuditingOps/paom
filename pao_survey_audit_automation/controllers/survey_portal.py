63d0d775795a8c309aba2903c661344f42abbb03
Manuel Uzueta <muzueta@pao-mx.com>
2026-08-07 12:48:32 -0700

    test

--- pao_survey_audit_automation/controllers/survey_portal.py
+++ pao_survey_audit_automation/controllers/survey_portal.py
@@ -5,15 +5,17 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class SurveyAudit(Survey):
+class SurveyAuditController(Survey):
     
-    def survey_start(self, survey_id, **post):
-        """Captura respondent_email antes de iniciar la encuesta"""
-        # Captura el email si viene en los parámetros
+    def survey_start(self, survey_token, answer_token=None, email=False, **post):
+        """
+        Extiende survey_start para capturar el email del respondent.
+        """
+        # Captura respondent_email de los parámetros POST/GET
         respondent_email = post.get('respondent_email')
         if respondent_email:
             request.session['survey_respondent_email'] = respondent_email
             _logger.info("Survey respondent email captured: %s", respondent_email)
         
-        # Llama al método original
-        return super().survey_start(survey_id, **post)
+        # Llama al método original con todos sus parámetros
+        return super().survey_start(survey_token, answer_token=answer_token, email=email, **post)
