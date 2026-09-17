b0b3824310844d4267f761d2bb6f71fb8a6fe727
Hector <hctorcorts16@gmail.com>
2026-09-17 15:53:01 -0700

    test

---
+++ osp_management portal_only/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'OSP Management Portal Only',
+    'version': '17.0.1.0.0',
+    'category': 'Operations/OSP',
+    
+    'description': """
+        
+    """,
+    'author': 'Hector Cortes',
+    
+    'depends': ['base', 'mail', 'portal', 'website', 'web', 'osp_management' ],
+    'data': [
+        'views/osp_users_views.xml',
+        'views/osp_portal_templates.xml',
+    ],
+    
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
--- osp_management/models/__init__.py
+++ osp_management/models/__init__.py
@@ -1,3 +1,2 @@
 from . import osp_request
 from . import osp_form_template
-from . import res_users
